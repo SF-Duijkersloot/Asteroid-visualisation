f1b0a3cc0e248fb85ad21f9d8ad676013c139163
Sep Duijkersloot <Sep.duijkersloott@gmail.com>
2024-11-05 14:37:08 +0100

    animation code cleanup

--- src/lib/components/Asteroid.svelte
+++ src/lib/components/Asteroid.svelte
@@ -1,17 +1,26 @@
 <script>
-    export let asteroid;
+    import * as d3 from 'd3'
+    import { onMount } from 'svelte';
+    export let asteroid
 
-    $: ({ scaledCircleRadius, angle, orbitalRadius, is_potentially_hazardous_asteroid } = asteroid);
+    $: ({ scaledCircleRadius, angle, orbitalRadius, is_potentially_hazardous_asteroid } = asteroid)
 
-    $: cx = orbitalRadius * Math.cos(angle);
-    $: cy = orbitalRadius * Math.sin(angle);
+    $: cx = orbitalRadius * Math.cos(angle)
+    $: cy = orbitalRadius * Math.sin(angle)
+
+    let circleElement
+
+    onMount(() => {
+        d3.select(circleElement)
+            .datum(asteroid)
+    })
 </script>
 
 <circle 
+    bind:this={circleElement}
     {cx}
     {cy}
     r={scaledCircleRadius}
     fill={is_potentially_hazardous_asteroid ? '#CC4243' : '#DDDCDD'}
     class="asteroid"
-    data-hazardous="{is_potentially_hazardous_asteroid}"
 />
--- src/lib/utils/animations.js
+++ src/lib/utils/animations.js
@@ -2,22 +2,18 @@ import * as d3 from 'd3'
 
 const fadeOutNonHazardousAsteroids = () => {
     d3.selectAll('.asteroid')
-        .filter(function() {
-            return this.getAttribute('data-hazardous') === "false"
-        })
+        .filter(d => !d.is_potentially_hazardous_asteroid)
         .transition()
-        .duration(1000)
-        .attr('opacity', 0)
+        .duration(1500)
+        .attr('opacity', 0.25)
         .attr('transform', 'scale(0)')
 }
 
 const fadeInNonHazardousAsteroids = () => {
     d3.selectAll('.asteroid')
-        .filter(function() {
-            return this.getAttribute('data-hazardous') === "false"
-        })
+        .filter(d => !d.is_potentially_hazardous_asteroid)
         .transition()
-        .duration(1000)
+        .duration(1500)
         .attr('opacity', 1)
         .attr('transform', 'scale(1)')
 }
